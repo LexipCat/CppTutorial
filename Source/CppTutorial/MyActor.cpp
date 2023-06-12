591580f924c4fc89578e9cd20ec4d3d94a7c05db
ksw617 <ksw617@gmail.com>
2023-06-12 21:43:11 +0900

    update

--- Source/CppTutorial/MyActor.cpp
+++ Source/CppTutorial/MyActor.cpp
@@ -15,9 +15,6 @@ AMyActor::AMyActor()
 void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Log, TEXT("Log"));
-	UE_LOG(LogTemp, Warning, TEXT("Warning"));
-	UE_LOG(LogTemp, Error, TEXT("Error"));
 	
 }
 
@@ -25,6 +22,7 @@ void AMyActor::BeginPlay()
 void AMyActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	UE_LOG(LogTemp, Log, TEXT("DeltaTime : %f"), DeltaTime);
 
 }
 
