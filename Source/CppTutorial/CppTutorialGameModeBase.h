197cae7a311d5c68a1f8f821c0873807ab475155
ksw617 <ksw617@gmail.com>
2023-06-12 21:08:38 +0900

    Initial commit

---
+++ Source/CppTutorial/CppTutorialGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CppTutorialGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPPTUTORIAL_API ACppTutorialGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
